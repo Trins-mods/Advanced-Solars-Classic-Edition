4a6b2c648c9856da203450b91da396fc93549bc7
trinsdar <30245301+Trinsdar@users.noreply.github.com>
2019-01-21 23:25:54 -0500

    finished recipes and removed a couple config entries

--- src/main/java/trinsdar/advancedsolars/util/AdvancedSolarsRecipes.java
+++ src/main/java/trinsdar/advancedsolars/util/AdvancedSolarsRecipes.java
@@ -24,32 +24,19 @@ public class AdvancedSolarsRecipes {
         recipes.addRecipe(new ItemStack(Registry.reinforcedIridiumIronPlate), "aca", "cic", "aca", 'a', Ic2Items.advancedAlloy, 'c', Ic2Items.carbonPlate, 'i', Registry.iridiumIronPlate);
         recipes.addRecipe(new ItemStack(Registry.irradiantReinforcedPlate), "rsr", "lRl", "rdr", 'r', "dustRedstone", 's', Registry.sunnariumPart, 'l', "dyeBlue", 'R', Registry.reinforcedIridiumIronPlate, 'd', "gemDiamond");
         recipes.addRecipe(new ItemStack(Registry.sunnariumPart), "ugu", 'u', Ic2Items.uuMatter, 'g', "dustGlowstone");
-        if (Config.classicRecipe){
-            recipes.addShapelessRecipe(new ItemStack(Registry.hybridSolarPanel, 8), Registry.ultimateHybridSolarPanel);
-            recipes.addRecipe(new ItemStack(Registry.ultimateHybridSolarPanel), "hhh", "hch", "hhh", 'h', Registry.hybridSolarPanel, 'c', "circuitAdvanced");
-            recipes.addRecipe(new ItemStack(Registry.advancedSolarHelmet), " a ", "cnc", "glg", 'a', Registry.advancedSolarPanel, 'c', "circuitAdvanced", 'n', Ic2Items.nanoHelmet, 'g', Ic2Items.doubleInsulatedGoldCable, 'l', Ic2Items.transformerLV);
-            recipes.addRecipe(new ItemStack(Registry.hybridSolarHelmet), " H ", "cqc", "ghg", 'H', Registry.hybridSolarPanel, 'c', "circuitAdvanced", 'q', Ic2Items.quantumHelmet, 'g', Ic2Items.glassFiberCable, 'h', Ic2Items.transformerHV);
-            recipes.addRecipe(new ItemStack(Registry.ultimateHybridSolarHelmet), " u ", "cqc", "ghg", 'u', Registry.ultimateHybridSolarPanel, 'c', "circuitAdvanced", 'q', Ic2Items.quantumHelmet, 'g', Ic2Items.glassFiberCable, 'h', Ic2Items.transformerHV);
-            recipes.addRecipe(new ItemStack(Registry.ultimateHybridSolarHelmet), "u", "h", 'u', Registry.ultimateHybridSolarPanel, 'h', Registry.hybridSolarHelmet);
-            if (Config.harderRecipes){
-                recipes.addRecipe(new ItemStack(Registry.irradiantUranium), " g ", "gug", " g ", 'u', Ic2Items.uraniumIngot, 'g', "dustGlowstone");
-                recipes.addRecipe(new ItemStack(Registry.enrichedSunnarium), "iii", "isi", "iii", 'i', Registry.irradiantUranium, 's', Registry.sunnarium);
-                recipes.addRecipe(new ItemStack(Registry.enrichedSunnariumAlloy), " e ", "ese", " e ", 'e', Registry.enrichedSunnarium, 's', Registry.sunnariumAlloy);
-                recipes.addRecipe(new ItemStack(Registry.irradiantGlassPane, 6), "rrr", "igi", "rrr", 'r', Ic2Items.reinforcedGlass, 'i', Registry.irradiantUranium, 'g', "dustGlowstone");
-                recipes.addRecipe(new ItemStack(Registry.advancedSolarPanel), "rrr", "asa", "cic", 'r', Registry.irradiantGlassPane, 'a', Ic2Items.advancedAlloy, 's', Ic2Items.solarPanel, 'c', "circuitAdvanced", 'i', Registry.irradiantReinforcedPlate);
-                recipes.addRecipe(new ItemStack(Registry.hybridSolarPanel), "ClC", "iai", "csc", 'C', Ic2Items.carbonPlate, 'l', "blockLapis", 'i', "plateIridium", 'a', Registry.advancedSolarPanel, 'c', "circuitAdvanced", 's', Registry.enrichedSunnarium);
-                recipes.addRecipe(new ItemStack(Registry.ultimateHybridSolarPanel), " l ", "cac", "scs", 'l', "blockLapis", 'c', Ic2Items.coalChunk, 'a', Registry.advancedSolarPanel, 's', Registry.enrichedSunnariumAlloy);
-            }else {
-                recipes.addRecipe(new ItemStack(Registry.advancedSolarPanel), "rrr", "asa", "cic", 'r', Ic2Items.reinforcedGlass, 'a', Ic2Items.advancedAlloy, 's', Ic2Items.solarPanel, 'c', "circuitAdvanced", 'i', Registry.irradiantReinforcedPlate);
-                recipes.addRecipe(new ItemStack(Registry.hybridSolarPanel), "ClC", "iai", "csc", 'C', Ic2Items.carbonPlate, 'l', "blockLapis", 'i', "plateIridium", 'a', Registry.advancedSolarPanel, 'c', "circuitAdvanced", 's', Registry.sunnarium);
-                recipes.addRecipe(new ItemStack(Registry.ultimateHybridSolarPanel), " l ", "cac", "scs", 'l', "blockLapis", 'c', Ic2Items.coalChunk, 'a', Registry.advancedSolarPanel, 's', Registry.sunnariumAlloy);
-            }
-        }
-        if (Config.ic2cRecipes){
-            recipes.addRecipe(new ItemStack(Registry.irradiantUranium), " g ", "gug", " g ", 'u', Ic2Items.uraniumIngot, 'g', "dustGlowstone");
-            recipes.addRecipe(new ItemStack(Registry.enrichedSunnarium), "iii", "isi", "iii", 'i', Registry.irradiantUranium, 's', Registry.sunnarium);
-            recipes.addRecipe(new ItemStack(Registry.enrichedSunnariumAlloy), " e ", "ese", " e ", 'e', Registry.enrichedSunnarium, 's', Registry.sunnariumAlloy);
-            recipes.addRecipe(new ItemStack(Registry.irradiantGlassPane, 6), "rrr", "igi", "rrr", 'r', Ic2Items.reinforcedGlass, 'i', Registry.irradiantUranium, 'g', "dustGlowstone");
+        recipes.addRecipe(new ItemStack(Registry.advancedSolarHelmet), " a ", "cnc", "glg", 'a', Registry.advancedSolarPanel, 'c', "circuitAdvanced", 'n', Ic2Items.nanoHelmet, 'g', Ic2Items.doubleInsulatedGoldCable, 'l', Ic2Items.transformerLV);
+        recipes.addRecipe(new ItemStack(Registry.hybridSolarHelmet), " H ", "cqc", "ghg", 'H', Registry.hybridSolarPanel, 'c', "circuitAdvanced", 'q', Ic2Items.quantumHelmet, 'g', Ic2Items.glassFiberCable, 'h', Ic2Items.transformerHV);
+        recipes.addRecipe(new ItemStack(Registry.irradiantUranium), "ugu", "geg", "ugu", 'u', Ic2Items.uuMatter, 'e', Ic2Items.enderPearlUraniumIngot, 'g', "dustGlowstone");
+        recipes.addRecipe(new ItemStack(Registry.enrichedSunnarium), "iii", "isi", "iii", 'i', Registry.irradiantUranium, 's', Registry.sunnarium);
+        recipes.addRecipe(new ItemStack(Registry.irradiantGlassPane, 6), "rrr", "igi", "rrr", 'r', Ic2Items.reinforcedGlass, 'i', Registry.irradiantUranium, 'g', "dustGlowstone");
+        recipes.addRecipe(new ItemStack(Registry.advancedSolarPanel), "rrr", "asa", "cic", 'r', Registry.irradiantGlassPane, 'a', Ic2Items.advancedAlloy, 's', Ic2Items.lvSolarPanel, 'c', "circuitAdvanced", 'i', Registry.irradiantReinforcedPlate);
+        recipes.addRecipe(new ItemStack(Registry.hybridSolarPanel), "CmC", "iai", "csc", 'C', Ic2Items.carbonPlate, 'm', Ic2Items.mvSolarPanel, 'i', Ic2Items.iridiumPlate, 'a', Registry.advancedSolarPanel, 'c', "circuitAdvanced", 's', Registry.enrichedSunnarium);
+        recipes.addRecipe(new ItemStack(Registry.ultimateHybridSolarPanel), " h ", "pHp", "scs", 'h', Ic2Items.hvSolarPanel, 'p', Ic2Items.plasmaCore, 'H', Registry.hybridSolarPanel, 's', Registry.enrichedSunnariumAlloy, 'c', Ic2Items.coalChunk);
+        recipes.addRecipe(new ItemStack(Registry.ultimateHybridSolarHelmet), " u ", "ihi", 'u', Registry.ultimateHybridSolarPanel, 'i', Ic2Items.iridiumPlate, 'h', Registry.hybridSolarHelmet);
+        if (Loader.isModLoaded("gtclassic")){
+            recipes.addRecipe(new ItemStack(Registry.enrichedSunnariumAlloy), "pep", "ese", "pep", 'p', "itemPlasma", 'e', Registry.enrichedSunnarium, 's', Registry.sunnariumAlloy);
+        }else {
+            recipes.addRecipe(new ItemStack(Registry.enrichedSunnariumAlloy), "pep", "ese", "pep", 'p', Ic2Items.plasmaCell, 'e', Registry.enrichedSunnarium, 's', Registry.sunnariumAlloy);
         }
     }
 
--- src/main/java/trinsdar/advancedsolars/util/Config.java
+++ src/main/java/trinsdar/advancedsolars/util/Config.java
@@ -13,18 +13,11 @@ public class Config {
 
 
     private static final String CATEGORY_GENERAL = "general";
-
-    // general
+    
     public static float
     energyGeneratorSolarAdvanced = 1.0F,
     energyGeneratorSolarHybrid = 1.0F,
     energyGeneratorSolarUltimateHybrid = 1.0F;
-
-    // generation
-    public static boolean classicRecipe= false;
-    public static boolean ic2cRecipes = true;
-    public static boolean harderRecipes = true;
-
     public static void readConfig() {
         Configuration cfg = CommonProxy.config;
         try {
@@ -40,14 +33,6 @@ public class Config {
     }
 
     private static void initGeneralConfig(Configuration cfg) {
-        cfg.addCustomCategoryComment(CATEGORY_GENERAL, "General");
-        classicRecipe = cfg.getBoolean("classicRecipes", CATEGORY_GENERAL, classicRecipe,
-                "Enables recipes based off ones from 1.4.7");
-        ic2cRecipes = cfg.getBoolean("ic2cRecipes", CATEGORY_GENERAL, ic2cRecipes,
-                "Enables Recipes that use items from ic2 classic such as compact solar panels");
-        harderRecipes = cfg.getBoolean("harderRecipes", CATEGORY_GENERAL, harderRecipes,
-                "Enables Recipes based off the classic harder mode. Only applicable when classicRecipes is enabled.");
-
         energyGeneratorSolarAdvanced = cfg.getFloat("energyGeneratorSolarAdvanced", CATEGORY_GENERAL, energyGeneratorSolarAdvanced, 0.01F, 5.0F, "Base energy generation multiplier values for advanced solar - increase them for higher yeilds.");
         energyGeneratorSolarHybrid = cfg.getFloat("energyGeneratorSolarHybrid", CATEGORY_GENERAL, energyGeneratorSolarHybrid, 0.01F, 5.0F, "Base energy generation multiplier values for hybrid solar - increase them for higher yeilds.");
         energyGeneratorSolarUltimateHybrid = cfg.getFloat("energyGeneratorSolarUltimateHybrid", CATEGORY_GENERAL, energyGeneratorSolarUltimateHybrid, 0.01F, 5.0F, "Base energy generation multiplier values for ultimate hybrid - increase them for higher yeilds. Note: Higer values then 1.0 for this might not have an effect as the max input of the solar panel is 512/t");
